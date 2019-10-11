fdbb3b4576860e0af8ce889a7ee9144d6bae63e0
Eric Richards <esholundr@yahoo.com>
2019-10-11 10:07:15 -0600

    Added dependencies and reformatted App.js to be inside main index.js file, added other folders and files

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducer";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+import "./index.css";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+function App() {
+    return (
+        <div className="App">
+            <h1>SMURFS! 2.0 W/ Redux</h1>
+            <div>Welcome to your state management version of Smurfs!</div>
+            <div>Start inside of your `src/index.js` file!</div>
+            <div>Have fun!</div>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
